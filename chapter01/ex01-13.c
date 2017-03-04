51a173a454c747a262c8d62475596ce83282f2d0
sfdeloach <sfdeloach@gmail.com>
2017-03-03 22:08:04 -0500

    now with vertical histogram display

--- chapter01/ex01-13.c
+++ chapter01/ex01-13.c
@@ -7,14 +7,14 @@
 */
 
 #include<stdio.h>
-#define ARRAY_SIZE 20
+#define ARRAY_SIZE 25
 
-main()
+int main(void)
 {
-    int c, i, length, longest, wordarray[ARRAY_SIZE];
+    int c, i, length, longest, largestBin, wordarray[ARRAY_SIZE];
     
-    // Initializations
-    length = longest = 0;
+    // Initialize the array buckets to zero
+    length = longest = largestBin = 0;
     for (c = 0; c < ARRAY_SIZE; ++c) {
         wordarray[c] = 0;
     }
@@ -29,6 +29,7 @@ main()
             else {
                 ++wordarray[ARRAY_SIZE - 1];
                 length = 0;
+                printf("WARNING: size of wordarray is too small!!!");
             }
         }
         else {
@@ -43,17 +44,49 @@ main()
         }
     }
     
+    // Find the bin with the most words
+    for (i = 0; i < ARRAY_SIZE; i++) {
+        if (largestBin < wordarray[i]) {
+            largestBin = wordarray[i];
+        }
+    }
+
     // Print results as horizontal bars
+    printf("\n\nHISTOGRAM RESULTS:\n");
     for (c = longest; c > 0; --c) {
         printf("%3d \u2502 ", c);
         for (i = 0; i < wordarray[c]; ++i) {
-            printf("\u2501");
+            printf("\u2586");
         }
         printf("\n");
     }
     printf("\n");
     
     // Print results as vertical bars
-    
+    for (i = largestBin; i > 0; i--) {
+        for (c = 1; c < longest + 1; c++) {
+            if (wordarray[c] >= i) {
+                printf("  %s", "\u2588");
+            } else {
+                printf("   ");
+            }
+        }
+        printf("\n");
+    }
+    for (c = 1; c < 3 * longest + 1; c++) {
+        printf("\u2500");
+    }
+    printf("\n");
+    for (c = 1; c < longest + 1; c++) {
+        printf("%3d", c);
+    }
+    printf("\n\n");
 }
+
+
+
+
+
+
+
 
